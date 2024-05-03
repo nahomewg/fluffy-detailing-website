412040898800968a28ae9477bed0c8e633cd88b0
Nahome Wolde-Giorgis <woldegio@ualberta.ca>
2024-05-02 23:55:48 -0600

    pricing, gallery, and proper routing.

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 // Index Javascript file, housing the functions of the web page
 window.addEventListener("load", (event) => {
+    currentPage = "home";
     sections = document.querySelectorAll("section");
     navTab = document.querySelectorAll("nav .icons div");
     currentTab = document.querySelectorAll("nav .icons div.active");
+
+    const urlParams = new URLSearchParams(window.location.search);
+    const scrollSection = urlParams.get('scrollSection');
+
+    if (scrollSection) {
+        toSection(scrollSection);
+    }
 
     // window.addEventListener("scroll", (event) => {
     //         sections.forEach((section) => {
@@ -28,24 +36,28 @@ function scrollFunction() {
 }
 
 // Scroll to function, to smoothly navigate through the web page.
-function toSection(section, tabId = '') {
+function toSection(section, tabId = '', navPage = false) {
     const element = document.getElementById(section);
     element.scrollIntoView( { behavior: "smooth" } );
 
-    if (window.screen.width <= 820 && tabId != '') {
+    if (window.screen.width <= 820 || tabId != '') {
         changeMobileTab(tabId);
     }
 
     if (tabId === '') {
-        changeMobileTab("home");
+        changeMobileTab(currentPage);
     }
 }
 
-function changeMobileTab(tabId) {
+function changeMobileTab(tabId, navPage) {
     navTab.forEach((tab) => {
         tab.classList.remove("active");
         if (tab.id === tabId) {
             tab.classList.add("active");
+            if (navPage) {
+                window.location.href = "./" + (tabId === 'home' ? 'index' : tabId === 'contact' ? 'pricing' : tabId) + ".html" 
+                + (tabId === 'contact' ? '?scrollSection=contact' : '');
+            }
         }
     });
 }
