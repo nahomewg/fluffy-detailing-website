8f4e5a7da30423777703e32bd5acf772060bf61e
Nahome Wolde-Giorgis <woldegio@ualberta.ca>
2024-05-07 13:37:54 -0600

    add gallery component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from '../components/home/home.component';
 import { PricingComponent } from '../components/pricing/pricing.component';
+import { GalleryComponent } from '../components/gallery/gallery.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full'},
   { path: 'about', redirectTo: '/home' },
   { path: 'home', component: HomeComponent },
-  { path: 'pricing', component: PricingComponent}
+  { path: 'pricing', component: PricingComponent},
+  { path: 'gallery', component: GalleryComponent}
 ];
