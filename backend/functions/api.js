b2ceaf39374fe4c2c76579a9e50728aa1c3c4c02
Nahome Wolde-Giorgis <woldegio@ualberta.ca>
2024-07-03 22:00:52 -0600

    production deployment of express/mongodb server

--- backend/functions/api.js
+++ backend/functions/api.js
@@ -1,11 +1,37 @@
 const express = require('express');
+const mongoose = require("mongoose");
 const serverless = require('serverless-http');
 const app = express();
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('App is running..');
-  });
-  
+const connectionString = process.env.ATLAS_URI || "";
+const AppointmentRequest = require("../models/appointmentRequest.model");
+
+app.get("/api/appointmentRequest", async (req, res) => {
+    try {
+        const appointments = await AppointmentRequest.find({});
+        res.status(200).json(appointments);
+    } catch(error) {
+        res.status(500).json({message: error.message});
+    }
+})
+
+/**
+ * Connect to the database and listen for the locahost:3000 port. Try/catch for error handling.
+ * @param {any}
+ * @returns {any}
+ */
+mongoose.connect(connectionString)
+.then(() => {
+    console.log("Connected to database!");
+    })
+    .catch(() => {
+        console.log("Connection failed!");
+    });
+
 app.use('/.netlify/functions/api', router);
-module.exports.handler = serverless(app);
+const handler = serverless(app);
+module.exports.handler = async (event, context) => {
+    const result = await handler(event, context);
+    return result;
+}
