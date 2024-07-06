282ab0ed8559dd30c02f05b66e82482a0386bc82
Nahome Wolde-Giorgis <woldegio@ualberta.ca>
2024-07-05 19:12:51 -0600

    delete backend server

--- backend/functions/triggerAppointmentEmail.js
+++
@@ -1,9 +0,0 @@
-const fetch = require('node-fetch');
-
-// automatically generate snippet from the email preview
-// sends a reques tto an email handler for an appointment
-await fetch(`${process.env.URL}/.netlify/functions/appointmentEmail`, {
-    headers: {
-        "netlify-emails-secret": process.env.NETLIFY_EMAILS_SECRET
-    }
-});
